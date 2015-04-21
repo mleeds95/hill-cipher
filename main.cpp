a9e3919a6e02296b9f6bb126d60580d4516f7f88
mleeds95 <mwl458@gmail.com>
2015-04-21 18:17:28 -0500

    Encryption is working for input text of arbitrary length.

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 // These functions convert "0-28" to "A-Z,. " and vice versa.
 void mapIntegersToCharacters(const int* arr, int size);
-tuple<int*, int> mapCharactersToIntegers();
+tuple<int**, int> mapCharactersToIntegers(int n);
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
@@ -37,37 +37,50 @@ int main(int argc, char* argv[]) {
     const char* filename = argv[2];
     ifstream inFile(filename);
     if (!inFile.is_open()) {
-        cerr << "Error: File '" << filename << "' not found or inaccessible.\n";
+        cerr << "Error: File '" << filename << "' not found or inaccessible." << endl;
         return 2;
     }
     // Assume the data file is formatted correctly.
     string line1;
     inFile >> line1;
     int n = atoi(line1.substr(2).c_str()); // it's an n by n matrix
+    if (n > 3 || n < 2) {
+        cerr << "Error: Currently n=2 and n=3 are the only supported block sizes." << endl;
+        inFile.close();
+        return 3;
+    }
     Matrix* keyMatrix = new Matrix(inFile, n);
     inFile.close();
     // At this point, keyMatrix should be initialized.
-    cout << *keyMatrix << endl;
-    //Matrix* inv = keyMatrix->findGaussJordanInverse();
-    //cout << *inv << endl;
-    //const int arr[] = {7,4,11,11,14,28};
-    //mapIntegersToCharacters(arr, 6);
-    //TODO encrypt/decrypt
-    if (ENCRYPT) {
-        tuple<int*,int> input = mapCharactersToIntegers();
-        int size = get<1>(input);
-        int* arr = get<0>(input);
-        int* encrypted = keyMatrix->multiplyMod29(arr);
-        for (int i = 0; i < n; ++i) {
-            cout << encrypted[i] << endl;
+    if (ENCRYPT) { // encrypt stdin -> stdout
+        tuple<int**, int> input = mapCharactersToIntegers(n);
+        // m is the number of n-length substrings the input text was broken into
+        int m = get<1>(input); 
+        int** plaintext = get<0>(input);
+        int** encrypted = new int*[m];
+        for (int i = 0; i < m; ++i) {
+            // encrypt each by multiplying by the key matrix
+            encrypted[i] = keyMatrix->multiplyMod29(plaintext[i]);
+            // write the result to stdout as characters
+            mapIntegersToCharacters(encrypted[i], n);
         }
-        delete arr;
+        cout << endl;
+        for (int i = 0; i < m; ++i)
+            delete[] encrypted[i];
         delete encrypted;
+        for (int i = 0; i < m; ++i)
+            delete[] plaintext[i];
+        delete plaintext;
+    } else { // decrypt stdin -> stdout
+        //Matrix* inv = keyMatrix->findGaussJordanInverse();
+        //cout << *inv << endl;
+        //TODO
     }
     delete keyMatrix;
     return 0;
 }
 
+// map integers to characters to integers as defined and write to stdout
 void mapIntegersToCharacters(const int* arr, int size) {
     for (int i = 0; i < size; ++i) {
         if (arr[i] < 26 && arr[i] >= 0) cout << (char) (arr[i] + 65);
@@ -78,19 +91,29 @@ void mapIntegersToCharacters(const int* arr, int size) {
     }
 }
 
-tuple<int*, int> mapCharactersToIntegers(int n) {
+// map characters from stdin to integer arrays based on predefined mappings
+tuple<int**, int> mapCharactersToIntegers(int n) {
     string inText;
     cin >> inText;
-    int size = inText.length();
-    int* arr = new int[size];
-    for (int i = 0; i < size; ++i) {
-        char c = inText.at(i);
-        if (c >= 'A' && c <= 'Z') arr[i] = ((int) c) - 65;
-        else if (c == '.') arr[i] = 26;
-        else if (c == ',') arr[i] = 27;
-        else if (c == ' ') arr[i] = 28;
-        else arr[i] = -1;
+    // pad until it's a multiple of n
+    while (inText.length() % n != 0) {
+        inText += " ";
     }
-    tuple<int*, int> returnVal(arr, size);
+    int m = inText.length() / n;
+    // allocate an array for the m substrings of length n
+    int** arr = new int*[m];
+    // iterate over the substrings, writing appropriate integers to arr
+    for (int i = 0; i < m; ++i) {
+        arr[i] = new int[n];
+        for (int j = 0; j < n; ++j) {
+            char c = inText.at(i*n + j);
+            if (c >= 'A' && c <= 'Z') arr[i][j] = ((int) c) - 65;
+            else if (c == '.') arr[i][j] = 26;
+            else if (c == ',') arr[i][j] = 27;
+            else if (c == ' ') arr[i][j] = 28;
+            else arr[i][j] = -1; // invalid character
+        }
+    }
+    tuple<int**, int> returnVal(arr, m);
     return returnVal;
 }
